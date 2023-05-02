6e537755a076f669cfc80746206b6f026119a748
Ivo <ivo.kraev2@protonmail.com>
2023-05-02 12:22:11 +0300

    Add service props

--- src/app/calculator/calculator.service.ts
+++ src/app/calculator/calculator.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Subject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculatorService {
+
+  devices: any[] = [];
+
+  electricityConsummationPerMonth = new Subject<any>();
+  electricityPricePerMonth = new Subject<any>();
+  electricityDeviceCostForLifetime = new Subject<any>();
+  carbonFootprint = new Subject<any>();
+  energyEfficiency = new Subject<any>();
 
   constructor() { }
 }
