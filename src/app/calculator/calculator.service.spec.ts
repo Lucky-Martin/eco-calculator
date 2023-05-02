29b8ebfdccdcec448442654cea2fe85c9303a7fe
Ivo <ivo.kraev2@protonmail.com>
2023-05-02 20:24:47 +0300

    Add baseDevice for testing

--- src/app/calculator/calculator.service.spec.ts
+++ src/app/calculator/calculator.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
+import type { IDevice } from "../../models/device.model";
 
 describe('CalculatorService', () => {
   let service: CalculatorService;
+  const baseDevice: IDevice = {
+    power: 5,
+    hoursPerMonth: 20,
+    energyClass: "APP",
+    typeOfDevice: "refrigerator",
+    warrantyInMonths: 24
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
