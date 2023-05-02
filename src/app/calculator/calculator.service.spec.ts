9dce3f04bd11793867b6ee7ebe5059433bb766c4
Ivo <ivo.kraev2@protonmail.com>
2023-05-02 21:51:58 +0300

    Update tests

--- src/app/calculator/calculator.service.spec.ts
+++ src/app/calculator/calculator.service.spec.ts
@@ -5,6 +5,7 @@ import type { INewDevice } from "../models/device.model";
 
 describe('CalculatorService', () => {
   let service: CalculatorService;
+  let emittedValue!: number;
   const baseDevice: INewDevice = {
     power: 0.13,
     hoursPerMonth: 600,
@@ -16,6 +17,7 @@ describe('CalculatorService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(CalculatorService);
+    service.setDevice(baseDevice);
   });
 
   it('should be created', () => {
@@ -23,25 +25,19 @@ describe('CalculatorService', () => {
   });
 
   it('should calculate energy consumption', () => {
-    let emittedValue!: number;
     service.electricityConsummationPerMonth.subscribe((value) => emittedValue = value);
-    service.setDevice(baseDevice);
     service.calculateElectricityConsummationPerMonth();
     expect(emittedValue).toEqual(78)
   });
 
   it('should calculate electricity device cost for month', () => {
-    let emittedValue!: number;
     service.electricityDeviceCostForMonth.subscribe((value) => emittedValue = value);
-    service.setDevice(baseDevice);
     service.calculateElectricityDeviceCostForMonth();
     expect(emittedValue).toEqual(17.706)
   });
 
   it('should calculate electricity consumption for the lifetime of the device', () => {
-    let emittedValue!: number;
     service.electricityDeviceCostForLifetime.subscribe((value) => emittedValue = value);
-    service.setDevice(baseDevice);
     service.calculateElectricityDeviceCostForLifetime();
     expect(emittedValue).toEqual(424.944)
   });
