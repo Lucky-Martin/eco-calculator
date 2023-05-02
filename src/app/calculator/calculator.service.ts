ff3f723275cbb0c876e5d8c5744f6ff3d008c686
Ivo <ivo.kraev2@protonmail.com>
2023-05-02 12:30:21 +0300

    Add functions for calculating values

--- src/app/calculator/calculator.service.ts
+++ src/app/calculator/calculator.service.ts
@@ -15,4 +15,34 @@ export class CalculatorService {
   energyEfficiency = new Subject<any>();
 
   constructor() { }
+
+  addDevice(newDevice: any){
+    this.devices.push(newDevice);
+  }
+
+  calculateEverything() {
+    this.calculateElectricityConsummationPerMonth();
+    this.calculateElectricityPricePerMonth();
+  }
+
+  calculateElectricityConsummationPerMonth() {
+    this.electricityConsummationPerMonth.next(1);
+  }
+
+  calculateElectricityPricePerMonth() {
+    this.electricityPricePerMonth.next(1);
+  }
+
+  calculateElectricityDeviceCostForLifetime() {
+    this.electricityDeviceCostForLifetime.next(1);
+
+  }
+
+  calculateCarbonFootprint() {
+    this.carbonFootprint.next(1);
+  }
+
+  calculateEnergyEfficiency() {
+    this.energyEfficiency.next(1);
+  }
 }
