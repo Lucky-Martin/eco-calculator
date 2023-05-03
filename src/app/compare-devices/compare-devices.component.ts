e6c59426c68acd6bc92fe078c0054cae733df612
Ivo <ivo.kraev2@protonmail.com>
2023-05-03 22:25:08 +0300

    Add input logic for the two compare devices

--- src/app/compare-devices/compare-devices.component.ts
+++ src/app/compare-devices/compare-devices.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DeviceService} from "../services/device.service";
 import { IDevice } from '../models/device.model';
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-compare-devices',
@@ -9,9 +10,27 @@ import { IDevice } from '../models/device.model';
 })
 export class CompareDevicesComponent implements OnInit {
   devices!: IDevice[];
-  constructor(private deviceService: DeviceService) { }
+  firstDeviceInput: FormControl
+  secondDeviceInput: FormControl;
+
+  firstDevice!: IDevice;
+  secondDevice!: IDevice;
+
+
+  constructor(private deviceService: DeviceService) {
+    this.firstDeviceInput = new FormControl();
+    this.secondDeviceInput = new FormControl();
+  }
 
   ngOnInit(): void {
     this.devices = this.deviceService.fetchDevices();
+  }
+
+  onSelectFirstDevice(uuid: string){
+    this.firstDevice = this.deviceService.getDevice(uuid)!;
+  }
+
+  onSelectSecondDevice(uuid: string){
+    this.secondDevice = this.deviceService.getDevice(uuid)!;
   }
 }
