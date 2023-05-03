73cc2cd17588bcb28cfe38e1aef33f57a7cb08c9
Ivo <ivo.kraev2@protonmail.com>
2023-05-03 19:27:54 +0300

    Create enum for cost per kilowatt-hour

---
+++ src/app/enums/costPerKilowattHour.ts
@@ -0,0 +1,13 @@
+export enum CostPerKilowattHour {
+  ElectroholdDay = 0.11146,
+  ElectroholdNight = 0.02629,
+
+  EVNBulgariaDay = 0.10917,
+  EVNBulgariaNight = 0.02115,
+
+  EnergoProDay = 0.10917,
+  EnergoProNight = 0.10917,
+
+  ESPGoldenSandsDay = 0.10429,
+  ESPGoldenSandsNight = 0.04557,
+}
