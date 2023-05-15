a266934fd15059f580e30b062087c714f627653b
Lucky-Martin <l.matev@casualino.com>
2023-05-15 11:25:29 +0300

    Implement table compare device view

--- src/app/compare-devices/compare-devices.component.ts
+++ src/app/compare-devices/compare-devices.component.ts
@@ -6,6 +6,7 @@ interface IDataItem {
   criteria: string;
   firstDeviceValue: string;
   secondDeviceValue: string;
+  objectKey: string;
 }
 
 @Component({
@@ -18,8 +19,8 @@ export class CompareDevicesComponent implements OnInit {
   dataSource!: IDataItem[];
   firstDevice!: IDevice | null;
   secondDevice!: IDevice | null;
-  firstDeviceHighlight!: boolean;
-  secondDeviceHighlight!: boolean;
+  firstDeviceHighlight!: string[] | null;
+  secondDeviceHighlight!: string[] | null;
   showDifferences = false;
   reset = false;
   displayedColumns: string[] = [];
@@ -53,18 +54,18 @@ export class CompareDevicesComponent implements OnInit {
 
     const diff = this.deviceService.compareDevice(this.firstDevice!, this.secondDevice!);
     if (diff) {
-      this.firstDeviceHighlight = diff.betterInFirstDevice.length > diff.betterInSecondDevice.length;
-      this.secondDeviceHighlight = diff.betterInFirstDevice.length < diff.betterInSecondDevice.length;
+      this.firstDeviceHighlight = diff.betterInFirstDevice;
+      this.secondDeviceHighlight = diff.betterInSecondDevice;
 
       const firstFootprint = this.firstDevice!.carbonFootprint;
       const secondFootprint = this.secondDevice!.carbonFootprint;
       this.dataSource = [
-        {criteria: 'Консумация на електроенергия за периода', firstDeviceValue: `${firstFootprint.electricityConsummationPerMonth} кВч`, secondDeviceValue: `${secondFootprint.electricityConsummationPerMonth} кВч`},
-        {criteria: 'Разход за ел. енергия на периода', firstDeviceValue: `${firstFootprint.electricityDeviceCostForMonth} лв`, secondDeviceValue: `${secondFootprint.electricityDeviceCostForMonth} лв`},
-        {criteria: 'Разход на ел. енергия за целия живот на стоката', firstDeviceValue: `${firstFootprint.electricityDeviceConsumptionForLifetime} кВч`, secondDeviceValue: `${secondFootprint.electricityDeviceConsumptionForLifetime} кВч`},
-        {criteria: 'Разход за ел. енергия за целия живот на стоката', firstDeviceValue: `${firstFootprint.electricityDeviceCostForLifetime} лв`, secondDeviceValue: `${secondFootprint.electricityDeviceCostForLifetime} лв`},
-        {criteria: 'Въглероден отпечатък на ползване', firstDeviceValue: `${firstFootprint.carbonFootprint} C02/кВч`, secondDeviceValue: `${secondFootprint.carbonFootprint} C02/кВч`},
-        {criteria: 'Енергийна ефективност', firstDeviceValue: firstFootprint.energyEfficiency, secondDeviceValue: secondFootprint.energyEfficiency}
+        {criteria: 'Консумация на електроенергия за периода', firstDeviceValue: `${firstFootprint.electricityConsummationPerMonth} кВч`, secondDeviceValue: `${secondFootprint.electricityConsummationPerMonth} кВч`, objectKey: 'electricityConsummationPerMonth'},
+        {criteria: 'Разход за ел. енергия на периода', firstDeviceValue: `${firstFootprint.electricityDeviceCostForMonth} лв`, secondDeviceValue: `${secondFootprint.electricityDeviceCostForMonth} лв`, objectKey: 'electricityDeviceCostForMonth'},
+        {criteria: 'Разход на ел. енергия за целия живот на стоката', firstDeviceValue: `${firstFootprint.electricityDeviceConsumptionForLifetime} кВч`, secondDeviceValue: `${secondFootprint.electricityDeviceConsumptionForLifetime} кВч`, objectKey: 'electricityDeviceConsumptionForLifetime'},
+        {criteria: 'Разход за ел. енергия за целия живот на стоката', firstDeviceValue: `${firstFootprint.electricityDeviceCostForLifetime} лв`, secondDeviceValue: `${secondFootprint.electricityDeviceCostForLifetime} лв`, objectKey: 'electricityDeviceCostForLifetime'},
+        {criteria: 'Въглероден отпечатък на ползване', firstDeviceValue: `${firstFootprint.carbonFootprint} C02/кВч`, secondDeviceValue: `${secondFootprint.carbonFootprint} C02/кВч`, objectKey: 'carbonFootprint'},
+        {criteria: 'Енергийна ефективност', firstDeviceValue: firstFootprint.energyEfficiency, secondDeviceValue: secondFootprint.energyEfficiency, objectKey: 'energyEfficiency'}
       ];
 
       this.displayedColumns = ['criteria', this.firstDevice!.name, this.secondDevice!.name]
@@ -82,9 +83,9 @@ export class CompareDevicesComponent implements OnInit {
   clearDevices() {
     this.showDifferences = false;
     this.firstDevice = null;
-    this.firstDeviceHighlight = false;
+    this.firstDeviceHighlight = null;
     this.secondDevice = null;
-    this.secondDeviceHighlight = false;
+    this.secondDeviceHighlight = null;
     this.reset = true;
     this.devicesToChoose = this.allDevices;
 
