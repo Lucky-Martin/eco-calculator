855428d1370aa8f146a6cd75fd713c12593d21af
Ivo <ivo.kraev2@protonmail.com>
2023-05-17 22:40:28 +0300

    Create EPREL product type enum

--- src/app/models/device.model.ts
+++ src/app/models/device.model.ts
@@ -15,6 +15,34 @@ export type TDeviceType =
   | "boiler"
   | "lightsources";
 
+export enum EprelProductGroup{
+  dishwashers2019 = "Съдомиялна",
+  washingmachines2019 = "Пералня",
+  washerdriers2019 = "Сушилня",
+  electronicdisplays = "Телевизор/Монитор",
+  refrigeratingappliances2019 = "Хладилник/Фризер/Хранилище за вино",
+  refrigeratingappliancesdirectsalesfunction = "Хладилник/Фризер/Охладител за напитки/Фризер за сладолед (търговска функция)",
+  tyres = "Гуми",
+  lightsources = "Светлинен източник",
+  airconditioners = "Климатик",
+  ovens = "Фурна",
+  rangehoods = "Аспиратор",
+  tumbledriers = "Домакинска барабанна сушилня",
+  localspaceheaters = "Отоплител",
+  professionalrefrigeratedstoragecabinets = "Професионален хладилен шкаф",
+  residentialventilationunits = "Жилищно вентилационно устройство",
+  solidfuelboilers = "Котел на твърдо гориво",
+  solidfuelboilerpackages = "Пакет от котли на твърдо гориво",
+  spaceheaters = "Пространствен/комбиниран отоплител",
+  spaceheaterpackages = "Пакет от пространствени/комбинирани отоплителни уреди",
+  spaceheatertemperaturecontrol = "Термостат/Терморегулатор",
+  spaceheatersolardevice = "",
+  waterheaters = "",
+  waterheaterpackages = "",
+  hotwaterstoragetanks = "",
+  waterheatersolardevices = ""
+}
+
 export type DeviceProp =
   "power"
   | "energyClass"
