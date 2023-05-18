9f09c4c3f62033c91a554272402a547ff75eb531
Lucky-Martin <luckymartinlc@gmail.com>
2023-05-18 03:19:17 +0300

    Update nav & add responsive queries

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  @Output('pageChanged') pageChanged: EventEmitter<void> = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onChangePage() {
+    this.pageChanged.emit();
   }
 
 }
